584fea5983afe7ac8f58741edb766eed32500e46
Ikechukwu Ajike <bestiyke@gmail.com>
2019-10-19 13:20:06 +0100

    first commit, initial setup

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const createHttpError = require('http-errors');
+const path = require('path');
+const config = require('./config/config');
+
+//impor routes
+const homeRoute = require('./routes/home')
+
+const PORT = process.env.PORT || config.PORT;
+
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', homeRoute);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    next(createHttpError(404, 'Not Found'));
+  });
+  
+  // error handler
+  app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+  
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error', {error: err});
+  });
+
+  app.listen(PORT, () => console.log('server started on port ' + PORT));
